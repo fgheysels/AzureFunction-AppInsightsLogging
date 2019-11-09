f7cc742a4c50671d38d373b785cf5585d51b89bc
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-11-09 23:00:34 +0100

    Add logging when things to ok

--- src/HttpFunction/LogTester.cs
+++ src/HttpFunction/LogTester.cs
@@ -1,16 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HttpFunction
 {
@@ -18,6 +17,8 @@ namespace HttpFunction
     {
         private static readonly TelemetryClient Logger
             = new TelemetryClient(new TelemetryConfiguration(Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY")));
+
+        private static readonly HttpClient Http = new HttpClient();
 
         [FunctionName("LogTester")]
         public static async Task<IActionResult> Run(
@@ -34,9 +35,27 @@ namespace HttpFunction
             {
                 Logger.TrackTrace("Number is not even", SeverityLevel.Error,
                     new Dictionary<string, string>() { { "number", number.ToString() } });
-            }
 
-            return new OkResult();
+                Logger.TrackException(new InvalidOperationException("Number not even"));
+
+                return new OkObjectResult("number not even");
+            }
+            else
+            {
+                await Http.GetAsync("http://google.com");
+
+                var trace = new TraceTelemetry("number is even", SeverityLevel.Information);
+
+                if (Activity.Current != null)
+                {
+                    trace.Context.Operation.Id = Activity.Current.RootId;
+                    trace.Context.Operation.ParentId = Activity.Current.ParentId;
+                }
+
+                Logger.TrackTrace(trace);
+
+                return new OkObjectResult("number even");
+            }
         }
     }
 }
